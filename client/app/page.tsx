1cd84fdddc97cd828768077363f6191e519c57a8
Harley Zapata <contacto@harleyzapata.life>
2024-09-13 20:52:38 -0500

    Add assets and global styles

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {Hero} from "@/componentes";
+import {Hero} from "../components";
 
 export default function Home() {
     return (
---
+++ client/components/Hero.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import {CustomButton} from "@/components";
+
+const Hero = () => {
+    const handleScroll = () => {}
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Rick And Morty Challenge
+                </h1>
+                <p className="hero__subtitle">
+                    Conoce a todos los personajes de la serie y diviértete filtrando por su nombre, especie, género y
+                    estado.
+                </p>
+                <CustomButton
+                    title={"Ver personajes"}
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+                </div>
+
+                <div className="hero__image-overlay"/>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
